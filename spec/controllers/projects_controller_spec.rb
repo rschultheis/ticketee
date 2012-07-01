8ddf5f946210213e25d74b6a95c6628fbe13d1ab
Robert Schultheis <robert.schultheis@gmail.com>
2012-07-01 16:28:35 -0400

    added hiding of projects from users without permission to view them:

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -22,12 +22,21 @@ describe ProjectsController do
       end
     end
 
+    it "can not access the show action" do
+      sign_in(:user, user)
+      get :show, :id => project.id
+      response.should redirect_to(projects_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+
   end
 
   it "displays an error for a missing Project" do
+    sign_in(:user, user)
     get :show, :id => 'not-here'
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found."
     flash[:alert].should eql(message)
   end
+
 end
