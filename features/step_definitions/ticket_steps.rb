b968f348c2c640bb861746f9ea1b0806d1c799f7
Robert Schultheis <robert.schultheis@gmail.com>
2012-07-02 01:17:56 -0400

    can set up all permissions

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -1,4 +1,4 @@
-And /^user "([^"]*)" has created a ticket for this project:$/ do |email, table|
+And /"([^"]*)" has created a ticket for this project:$/ do |email, table|
   user = User.find_by_email!(email)
   table.hashes.each do |attributes|
     @project.tickets.create!(attributes.merge!(:user => user))
